115bc11082418ad7a08b46944583c85fbccceebe
RennamFaria <nirram1234@gmail.com>
2024-05-28 00:20:45 -0300

    Enhancements to train and test

--- a2c/test.py
+++ a2c/test.py
@@ -1,19 +1,59 @@
+import argparse
 from stable_baselines3 import A2C
 from stable_baselines3.common.env_util import make_vec_env
 from env import make_env
 
-def main():
-    vec_env = make_vec_env(make_env)
+def main(args):
+        vec_env = make_vec_env(make_env)
+        vec_env.reset()
 
-    model = A2C.load("a2c_sf6a_final", env=vec_env)
+        # Loading model treined
+        model = A2C.load(args.model_load_path, env=vec_env)
 
-    print("Starting test")
-    done = False
-    obs = vec_env.reset()
-    while not done:
-        action, _states = model.predict(obs, deterministic=True)
-        obs, reward, done, info = vec_env.step(action)
-    vec_env.close()
+        print("Starting test")
+
+        # Test the model treined
+        total_rewards = []
+        best_reward = 0
+        best_action = []
+        for episode in range(args.num_episodes):
+            obs = vec_env.reset()
+            done = False
+            episode_reward = 0
+            episode_action = []
+
+            while not done:
+                action, _states = model.predict(obs, deterministic=False)
+                obs, reward, done, info = vec_env.step(action)
+
+                episode_action.append(action[0])
+                episode_reward += reward[0]
+
+            if episode_reward > best_reward:
+                best_reward = episode_reward
+                best_action = episode_action
+
+            print(f"Episode {episode + 1}")
+            print(f"\treward: {episode_reward}")
+            #print(f"\taction: {episode_action}")
+            total_rewards.append(episode_reward)
+
+        # Print results from test
+        print(f"Reward reults from {args.num_episodes} episodes:")
+        avg_reward = sum(total_rewards) / len(total_rewards)
+        std_reward = (sum((x - avg_reward) ** 2 for x in total_rewards) / len(total_rewards)) ** 0.5
+        print(f"avg_reward: {avg_reward:.2f}")
+        print(f"std_reward: {std_reward:.2f}")
+        print(f"\nBest reward: {best_reward}")
+        print(f"Best action: {best_action}\n")
+
+        vec_env.close()
 
 if __name__ == "__main__":
-     main()
+        parser = argparse.ArgumentParser(description="Run test from A2C model")
+
+        parser.add_argument("--num_episodes", type=int, default=100, help="Number of episodes to test")
+        parser.add_argument("--model_load_path", type=str, default="treined_models/a2c_sf6a_final", help="Path to load the trained model")
+        
+        args = parser.parse_args()
+        main(args)
--- a2c/train.py
+++ a2c/train.py
@@ -1,22 +1,52 @@
 from stable_baselines3 import A2C
 from stable_baselines3.common.env_util import make_vec_env
 from env import make_env
+import argparse
+import os
 
-def main():
-        env = make_env(False)
+def main(args):
+      env = make_env(args.render)
 
-        # Create a vectorized environment without rendering
-        vec_env = make_vec_env(lambda: env, n_envs=1)
+      # Create a vectorized environment
+      vec_env = make_vec_env(lambda: env, n_envs=args.n_envs)
+      
+      # Create the A2C model with TensorBoard logging enabled
+      model = A2C(
+         'CnnPolicy',
+         vec_env,
+         learning_rate=0.0007, 
+         n_steps=5, 
+         gamma=0.99, 
+         ent_coef=0.01, 
+         vf_coef=0.5, 
+         max_grad_norm=0.5, 
+         verbose=args.verbose
+         )
 
-        # Create the A2C model
-        model = A2C('CnnPolicy', vec_env, learning_rate=0.0007, n_steps=5, gamma=0.99, ent_coef=0.01, vf_coef=0.5, max_grad_norm=0.5, verbose=1)
+      # Train and save the model
+      time_steps = args.timesteps/args.qnt_saves
+      print(time_steps) 
+      for i in range(args.qnt_saves):
+         model.learn(total_timesteps=time_steps)
+         model_save_path = f"{args.model_save_path}/a2c_sf6a_{i*time_steps:.0f}"
+         model.save(model_save_path)
 
-        # Train the model with callbacks
-        model.learn(total_timesteps=10000)
+      # Save the final model
+      model_save_path = f"{args.model_save_path}/a2c_sf6a_final"
+      model.save(model_save_path)
 
-        # Save the model
-        model.save("a2c_sf6a_final")
-        vec_env.close()
+      vec_env.close()
 
 if __name__ == "__main__":
-     main()
+      parser = argparse.ArgumentParser(description="Train an A2C model on a custom environment")
+
+      # Optional arguments with defaults
+      parser.add_argument("--render", default='False', help="Choose to render the environment (True/False)")
+      parser.add_argument("--n_envs", type=int, default=1, help="Number of environments")
+      parser.add_argument("--verbose", type=int, default=1, help="Verbose mode (0: no output, 1: info)")
+      parser.add_argument("--timesteps", type=int, default=10000, help="Total timesteps of training")
+      parser.add_argument("--qnt_saves", type=int, default=5, help="Quantity of models will save")
+      parser.add_argument("--model_save_path", type=str, default="treined_models", help="Path to save the trained model")
+      
+      args = parser.parse_args()
+      main(args)
