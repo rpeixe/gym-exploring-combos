7fe240391656796d5f11f62f1664ed0012b2fed3
RennamFaria <nirram1234@gmail.com>
2024-06-19 16:55:21 -0300

    Added hyperparameter optimization, improvements

--- a2c/callbacks.py
+++ a2c/callbacks.py
@@ -1,22 +1,28 @@
 from stable_baselines3.common.callbacks import BaseCallback
-from stable_baselines3.common.logger import TensorBoardOutputFormat
+from stable_baselines3.common.logger import TensorBoardOutputFormat, HParam
 from torch.utils.tensorboard import SummaryWriter
 
 class TensorboardCallbackTrain(BaseCallback):
-      def __init__(self, verbose=0):
-        super(TensorboardCallbackTrain, self).__init__(verbose)
+    def _on_training_start(self) -> None:
+        hparam_dict = {
+            "algorithm": self.model.__class__.__name__,
+            "learning rate": self.model.learning_rate,
+            "gamma": self.model.gamma,
+        }
+        # define the metrics that will appear in the `HPARAMS` Tensorboard tab by referencing their tag
+        # Tensorbaord will find & display metrics from the `SCALARS` tab
+        metric_dict = {
+            "rollout/ep_len_mean": 0,
+            "train/value_loss": 0.0,
+        }
+        self.logger.record(
+            "hparams",
+            HParam(hparam_dict, metric_dict),
+            exclude=("stdout", "log", "json", "csv"),
+        )
 
-      def _on_training_start(self):
-        self._log_freq = 100     # log every 100 calls
-
-        output_formats = self.logger.output_formats
-
-        self.tb_formatter = next(formatter for formatter in output_formats if isinstance(formatter, TensorBoardOutputFormat))
-
-      def _on_step(self) -> bool:
-         #if self.n_calls % self._log_freq == 0:
-            
-         return True
+    def _on_step(self) -> bool:
+        return True
 
 class TensorboardCallbackTest(BaseCallback):
       def __init__(self, log_dir, verbose=0):
--- a2c/train.py
+++ a2c/train.py
@@ -1,6 +1,7 @@
 import argparse
 import os
-from stable_baselines3 import A2C, PPO
+import optuna
+from stable_baselines3 import A2C
 from stable_baselines3.common.callbacks import CallbackList, CheckpointCallback, EvalCallback
 from stable_baselines3.common.sb2_compat.rmsprop_tf_like import RMSpropTFLike
 from env import make_custom_vec_env
@@ -8,60 +9,88 @@ from callbacks import TensorboardCallbackTrain
 
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 
-def main(args):
+def main(trial, args):
     # Create a vectorized environment
-    vec_env = make_custom_vec_env(n_envs=args.n_envs, render=args.render, action_memory=args.no_action_memory)
-    eval_vec_env = make_custom_vec_env(n_envs=args.n_envs, render=args.render, action_memory=args.no_action_memory)
-    
-    # Dir to TensorBoard visualisation
-    for i in range(10):
-        log_dir = os.path.join(SCRIPT_DIR, f"logs/train/train{i+1}")
-        if not os.path.exists(log_dir):
-            os.makedirs(log_dir)
-            break
-        
+    vec_env = make_custom_vec_env(n_envs=1, render=False, action_memory=True)
+    eval_vec_env = make_custom_vec_env(n_envs=1, render=False, action_memory=True)
+
     policy_kwargs = {
         "optimizer_class": RMSpropTFLike,
         "optimizer_kwargs": {"eps": 1e-5}
     }
 
+    # Hyperparameters to optimize or preselected, don't save when optimizing
+    if args.n_optmization_trials > 0:
+        learning_rate = trial.suggest_float("learnig_rate", 1e-5, 1, log=True)
+        ent_coef = trial.suggest_float("ent_coef", 1e-8, 0.1, log=True)
+        vf_coef = trial.suggest_float('vf_coef', 1e-8, 0.1, log=True)
+        
+        path_models = None
+        path_results = None
+        log_dir = None
+    else:
+        learning_rate = 0.000219 
+        ent_coef = 1.262e-7
+        vf_coef = 0.000265
+
+        path_models=os.path.join(SCRIPT_DIR, "trained_models")
+        path_results=os.path.join(SCRIPT_DIR, "trained_models/results")
+
+        # Dir to TensorBoard visualisation
+        for i in range(10):
+            log_dir = os.path.join(SCRIPT_DIR, f"logs/train/train{i+1}")
+            if not os.path.exists(log_dir):
+                os.makedirs(log_dir)
+                break
+
     model = A2C(
         'MultiInputPolicy',
         vec_env,
-        ent_coef=0.01,
-        vf_coef=0.25,
+        learning_rate=learning_rate,
+        ent_coef=ent_coef,
+        vf_coef=vf_coef,
         policy_kwargs=policy_kwargs,
         seed=args.seed,
         verbose=args.verbose,
         tensorboard_log=log_dir
-        )
+    )
 
-    time_steps = args.timesteps/args.saves
-    
     # Callbacks for TensorBoard visualisation
-    checkpoint_callback = CheckpointCallback(
-        save_freq=max(int(time_steps), 1),
-        save_path=os.path.join(SCRIPT_DIR, "trained_models"),
-        name_prefix="a2c_sfa3",
-        verbose=2
-        )
-    
+    time_steps = args.timesteps/args.saves
+    callbackList = []
+
+    if args.n_optmization_trials <= 0:
+        checkpoint_callback = CheckpointCallback(
+            save_freq=max(int(time_steps), 1),
+            save_path=path_models,
+            name_prefix="a2c_sfa3",
+            verbose=2
+            )
+        callbackList.append(checkpoint_callback)
+        
     eval_callback = EvalCallback(
         eval_vec_env,
         n_eval_episodes=1,
         eval_freq=500,
-        log_path=os.path.join(SCRIPT_DIR, "trained_models/results"),
-        best_model_save_path=os.path.join(SCRIPT_DIR, "trained_models"),
+        log_path=path_results,
+        best_model_save_path=path_models,
         deterministic=True,
-        verbose=1
+        verbose=args.verbose
         )
+    callbackList.append(eval_callback)
     
     tensorboard_callback = TensorboardCallbackTrain()
-    callback = CallbackList([checkpoint_callback, eval_callback, tensorboard_callback])
+    callbackList.append(tensorboard_callback)
+
+    callback = CallbackList(callbackList)
+
     # Train, save the model and make Tensorboard visualisation
     model.learn(total_timesteps=args.timesteps, callback=callback)
 
+    eval_vec_env.close()
     vec_env.close()
+
+    return eval_callback.last_mean_reward 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Train an A2C model on a custom environment")
@@ -74,6 +103,17 @@ if __name__ == "__main__":
     parser.add_argument("-t", "--timesteps", type=int, default=10000, help="Total timesteps of training")
     parser.add_argument("--saves", type=int, default=5, help="Quantity of models will save")
     parser.add_argument("--seed", type=int, default=0, help="Define the random seed the model will use")
-    
+    parser.add_argument("-o", "--n_optmization_trials", type=int, default=100, help="Trials of hyperparameter optimization (0: preselect parms, 0>: optimize params)")
+
     args = parser.parse_args()
-    main(args)
+
+    if args.n_optmization_trials > 0:
+        study = optuna.create_study(direction='maximize')   
+        try:
+            study.optimize(lambda trial: main(trial, args),n_trials=args.n_optmization_trials)
+        except KeyboardInterrupt:
+            pass
+        print("Best hyperparameters: ", study.best_params)
+        print("Best reward: ", study.best_value)
+    else:
+       main(None, args)
